b127f0bdccf67dc20573888c96e932af04bcd715
software <407723273@qq.com>
2018-08-03 09:38:46 +0800

    大话设计模式

---
+++ 02策略模式/策略模式-商场管理软件06反射/商场管理软件/CashContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace 商场管理软件
+{
+    class CashContext
+    {
+        private CashSuper cs;
+
+        public void setBehavior(string className, object[] args)
+        {
+            this.cs = (CashSuper)Assembly.Load("商场管理软件").CreateInstance("商场管理软件." + className, false, BindingFlags.Default, null, args, null, null);
+        }
+
+        public double GetResult(double money)
+        {
+            return cs.acceptCash(money);
+        }
+    }
+}
